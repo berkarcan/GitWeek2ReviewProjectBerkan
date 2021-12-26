a4354e622c70e32324e9af202e873587c213b0af
Berkan Acar <berkanacar@gmail.com>
2021-12-26 20:58:12 +0100

    berkan still works

--- src/test/java/Berkanswork/berkanWork.java
+++ src/test/java/Berkanswork/berkanWork.java
@@ -3,5 +3,6 @@ package Berkanswork;
 public class berkanWork {
   public static void main(String[] args) {
     System.out.println("my first day oscars second");
+    System.out.println("still some job");
   }
 }
