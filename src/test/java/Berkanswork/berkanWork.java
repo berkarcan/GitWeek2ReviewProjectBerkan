2f19d580e1309c3189147364fd962d9ba75147d0
Berkan Acar <61150565+berkarcan@users.noreply.github.com>
2021-12-26 21:29:01 +0100

    Update berkanWork.java

--- src/test/java/Berkanswork/berkanWork.java
+++ src/test/java/Berkanswork/berkanWork.java
@@ -4,6 +4,6 @@ public class berkanWork {
   public static void main(String[] args) {
     System.out.println("my first day oscars second");
     System.out.println("still some job");
-    System.out.println("onother work");
+    System.out.println("onother work, but should be more");
   }
 }
